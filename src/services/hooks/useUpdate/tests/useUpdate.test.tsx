5ddd2d19f45ea67c7441e78f4f775e21e3931529
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2022-09-26 16:48:20 +0300

    Add `useUpdateOnlyExtended` hook and tests for it

--- src/services/hooks/useUpdate/tests/useUpdate.test.tsx
+++ src/services/hooks/useUpdate/tests/useUpdate.test.tsx
@@ -6,37 +6,31 @@ import { useUpdate } from '../useUpdate';
 
 describe('useUpdate', () => {
   it('Runs on mount and when dependency changes', () => {
-    const TestComponent: FC<{ callback: () => void; otherProp: string }> = ({
-      callback,
-      otherProp
-    }) => {
+    const TestComponent: FC<{ callback: () => void }> = ({ callback }) => {
       useUpdate(callback, [callback]);
 
-      return <div>{otherProp}</div>;
+      return null;
     };
 
     const callback = jest.fn();
 
-    const { rerender } = testRender(<TestComponent callback={callback} otherProp="valueA" />);
+    const { rerender } = testRender(<TestComponent callback={callback} />);
 
     expect(callback).toHaveBeenCalledTimes(1);
 
     const updatedCallback = jest.fn();
 
-    rerender(<TestComponent callback={updatedCallback} otherProp="valueA" />);
+    rerender(<TestComponent callback={updatedCallback} />);
 
     expect(callback).toHaveBeenCalledTimes(1);
     expect(updatedCallback).toHaveBeenCalledTimes(1);
   });
 
   it("Doesn't run when other prop changes", () => {
-    const TestComponent: FC<{ callback: () => void; otherProp: string }> = ({
-      callback,
-      otherProp
-    }) => {
+    const TestComponent: FC<{ callback: () => void; otherProp: string }> = ({ callback }) => {
       useUpdate(callback, [callback]);
 
-      return <div>{otherProp}</div>;
+      return null;
     };
 
     const callback = jest.fn();
@@ -55,27 +49,22 @@ describe('useUpdate', () => {
       callback,
       otherProp
     }) => {
-      useUpdate(callback, [callback]);
+      useUpdate(callback, [callback, otherProp]);
 
-      return <div>{otherProp}</div>;
+      return null;
     };
 
     const callbackReturn = jest.fn();
-    let callback = jest.fn(() => Promise.resolve(callbackReturn));
+    const callback = jest.fn(() => Promise.resolve(callbackReturn));
 
     const { rerender } = testRender(<TestComponent callback={callback} otherProp="valueA" />);
 
     expect(callback).toHaveBeenCalledTimes(1);
-    // TODO inner mock function not being called
-    // expect(callbackReturn).toHaveBeenCalledTimes(1);
-
-    const updatedCallbackReturn = jest.fn();
-    callback = jest.fn(() => updatedCallbackReturn) as jest.Mock;
 
     rerender(<TestComponent callback={callback} otherProp="valueB" />);
 
-    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledTimes(2);
     // TODO inner mock function not being called
-    // expect(updatedCallbackReturn).toHaveBeenCalledTimes(1);
+    // expect(callbackReturn).toHaveBeenCalledTimes(1);
   });
 });
--- src/services/hooks/useUpdateOnlyExtended/tests/useUpdateOnlyExtended.test.tsx
+++ src/services/hooks/useUpdateOnlyExtended/tests/useUpdateOnlyExtended.test.tsx
@@ -2,12 +2,25 @@ import { FC } from 'react';
 
 import { testRender } from '@services/utils';
 
-import { useUpdateExtended } from '../useUpdateExtended';
+import { useUpdateOnlyExtended } from '../useUpdateOnlyExtended';
 
-describe('useUpdateExtended', () => {
-  it('Runs on mount and when dependency changes', () => {
+describe('useUpdateOnlyExtended', () => {
+  it("Doesn't run on mount", () => {
     const TestComponent: FC<{ callback: () => void }> = ({ callback }) => {
-      useUpdateExtended(callback, [callback]);
+      useUpdateOnlyExtended(callback, [callback]);
+
+      return null;
+    };
+
+    const callback = jest.fn();
+
+    testRender(<TestComponent callback={callback} />);
+    expect(callback).toHaveBeenCalledTimes(0);
+  });
+
+  it('Runs the callback when dependency changes', () => {
+    const TestComponent: FC<{ callback: () => void }> = ({ callback }) => {
+      useUpdateOnlyExtended(callback, [callback]);
 
       return null;
     };
@@ -16,19 +29,17 @@ describe('useUpdateExtended', () => {
 
     const { rerender } = testRender(<TestComponent callback={callback} />);
 
-    expect(callback).toHaveBeenCalledTimes(1);
-
     const updatedCallback = jest.fn();
 
     rerender(<TestComponent callback={updatedCallback} />);
 
-    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledTimes(0);
     expect(updatedCallback).toHaveBeenCalledTimes(1);
   });
 
-  it("Doesn't run when other prop changes", () => {
+  it("Doesn't run the callback if other prop changes", () => {
     const TestComponent: FC<{ callback: () => void; otherProp: string }> = ({ callback }) => {
-      useUpdateExtended(callback, [callback]);
+      useUpdateOnlyExtended(callback, [callback]);
 
       return null;
     };
@@ -37,11 +48,9 @@ describe('useUpdateExtended', () => {
 
     const { rerender } = testRender(<TestComponent callback={callback} otherProp="valueA" />);
 
-    expect(callback).toHaveBeenCalledTimes(1);
-
     rerender(<TestComponent callback={callback} otherProp="valueB" />);
 
-    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledTimes(0);
   });
 
   it('Calls `callback` return function', () => {
@@ -49,7 +58,7 @@ describe('useUpdateExtended', () => {
       callback,
       otherProp
     }) => {
-      useUpdateExtended(callback, [callback, otherProp]);
+      useUpdateOnlyExtended(callback, [callback, otherProp]);
 
       return null;
     };
@@ -59,11 +68,11 @@ describe('useUpdateExtended', () => {
 
     const { rerender } = testRender(<TestComponent callback={callback} otherProp="valueA" />);
 
-    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledTimes(0);
 
     rerender(<TestComponent callback={callback} otherProp="valueB" />);
 
-    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenCalledTimes(1);
     // TODO inner mock function not being called
     // expect(callbackReturn).toHaveBeenCalledTimes(1);
   });
@@ -73,7 +82,7 @@ describe('useUpdateExtended', () => {
       callback,
       otherProp
     }) => {
-      useUpdateExtended(callback, [callback, otherProp], true);
+      useUpdateOnlyExtended(callback, [callback, otherProp], true);
 
       return null;
     };
@@ -82,9 +91,13 @@ describe('useUpdateExtended', () => {
 
     const { rerender } = testRender(<TestComponent callback={callback} otherProp={{ a: 1 }} />);
 
-    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledTimes(0);
 
     rerender(<TestComponent callback={callback} otherProp={{ a: 1 }} />);
+
+    expect(callback).toHaveBeenCalledTimes(0);
+
+    rerender(<TestComponent callback={callback} otherProp={{ a: 2 }} />);
 
     expect(callback).toHaveBeenCalledTimes(1);
   });
@@ -94,7 +107,7 @@ describe('useUpdateExtended', () => {
       callback,
       otherProp
     }) => {
-      useUpdateExtended(callback, [otherProp], ({ newValue }) =>
+      useUpdateOnlyExtended(callback, [otherProp], ({ newValue }) =>
         newValue.every((dep) => (dep as number) > 0)
       );
 
@@ -105,14 +118,14 @@ describe('useUpdateExtended', () => {
 
     const { rerender } = testRender(<TestComponent callback={callback} otherProp={1} />);
 
-    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledTimes(0);
 
     rerender(<TestComponent callback={callback} otherProp={2} />);
 
-    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenCalledTimes(1);
 
     rerender(<TestComponent callback={callback} otherProp={0} />);
 
-    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenCalledTimes(1);
   });
 });
